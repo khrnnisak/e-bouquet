7d1403a930aca7e4ce402a989e6c935b0118ce6f
Dka-11 <adikaahmad05@gmail.com>
2022-12-23 20:34:28 +0700

    edit code

--- lib/flutter-project/opening_page.dart
+++ lib/flutter-project/opening_page.dart
@@ -9,17 +9,15 @@ class OpeningPage extends StatelessWidget {
     double baseWidth = 428;
     double fem = MediaQuery.of(context).size.width / baseWidth;
     double ffem = fem * 0.97;
-    return Container(
+    return SizedBox(
       width: double.infinity,
       child: Container(
-        // openingpageSqL (21:327)
-        padding: EdgeInsets.fromLTRB(65*fem, 356*fem, 31*fem, 30*fem),
+        padding: EdgeInsets.fromLTRB(65 * fem, 356 * fem, 31 * fem, 30 * fem),
         width: double.infinity,
-        decoration: BoxDecoration (
-          color: Color(0xcc1e1e1e),
-          image: DecorationImage (
+        decoration: const BoxDecoration(
+          image: DecorationImage(
             fit: BoxFit.cover,
-            image: AssetImage (
+            image: AssetImage(
               'assets/flutter-project/images/b9c3fa2be3d5b0e2b89143ac40a2fb-1-bg.png',
             ),
           ),
@@ -28,46 +26,38 @@ class OpeningPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
-              // makeyourworldblossom4Lv (22:629)
-              margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 34*fem, 273*fem),
-              constraints: BoxConstraints (
-                maxWidth: 298*fem,
-              ),
+              margin: EdgeInsets.only(right: 34 * fem, bottom: 270 * fem),
               child: RichText(
                 textAlign: TextAlign.center,
                 text: TextSpan(
-                  style: SafeGoogleFont (
+                  style: SafeGoogleFont(
                     'Moon Dance',
-                    fontSize: 64*ffem,
+                    fontSize: 64 * ffem,
                     fontWeight: FontWeight.w400,
-                    height: 1.3400000334*ffem/fem,
-                    letterSpacing: -0.3000000119*fem,
-                    color: Color(0xffffffff),
+                    letterSpacing: -0.3000000119 * fem,
+                    color: const Color(0xffffffff),
                   ),
                   children: [
                     TextSpan(
                       text: 'Make',
-                      style: SafeGoogleFont (
+                      style: SafeGoogleFont(
                         'Mouse Memoirs',
-                        fontSize: 64*ffem,
+                        fontSize: 64 * ffem,
                         fontWeight: FontWeight.w400,
-                        height: 1.3400000334*ffem/fem,
-                        letterSpacing: -0.3000000119*fem,
-                        color: Color(0xffffffff),
+                        color: const Color(0xffffffff),
                       ),
                     ),
-                    TextSpan(
+                    const TextSpan(
                       text: ' your world ',
                     ),
                     TextSpan(
                       text: 'blossom',
-                      style: SafeGoogleFont (
+                      style: SafeGoogleFont(
                         'Mouse Memoirs',
-                        fontSize: 64*ffem,
+                        fontSize: 64 * ffem,
                         fontWeight: FontWeight.w400,
-                        height: 1.3400000334*ffem/fem,
-                        letterSpacing: -0.3000000119*fem,
-                        color: Color(0xffffffff),
+                        height: 1.3400000334 * ffem / fem,
+                        color: const Color(0xffffffff),
                       ),
                     ),
                   ],
@@ -75,26 +65,22 @@ class OpeningPage extends StatelessWidget {
               ),
             ),
             Container(
-              // buttoniVL (22:652)
-              margin: EdgeInsets.fromLTRB(140*fem, 0*fem, 0*fem, 0*fem),
-              width: 192*fem,
-              height: 52*fem,
-              decoration: BoxDecoration (
-                color: Color(0xfffd0079),
-                borderRadius: BorderRadius.circular(6*fem),
+              margin: EdgeInsets.only(left: 140 * fem),
+              height: 50 * fem,
+              decoration: BoxDecoration(
+                color: const Color(0xfffd0079),
+                borderRadius: BorderRadius.circular(10 * fem),
               ),
               child: Center(
                 child: Center(
                   child: Text(
                     'Mulai',
                     textAlign: TextAlign.center,
-                    style: SafeGoogleFont (
+                    style: SafeGoogleFont(
                       'Montserrat',
-                      fontSize: 20*ffem,
+                      fontSize: 20 * ffem,
                       fontWeight: FontWeight.w800,
-                      height: 1.218999958*ffem/fem,
-                      letterSpacing: -0.3*fem,
-                      color: Color(0xffffffff),
+                      color: const Color(0xffffffff),
                     ),
                   ),
                 ),
@@ -103,6 +89,6 @@ class OpeningPage extends StatelessWidget {
           ],
         ),
       ),
-          );
+    );
   }
 }
