69111eae3da95c054524b10b5f6216d772003ef5
Dka-11 <adikaahmad05@gmail.com>
2022-12-25 19:13:34 +0700

    edit homepage

--- lib/widgets/homeListItem.dart
+++ lib/widgets/homeListItem.dart
@@ -10,131 +10,101 @@ class HomeListItem extends StatelessWidget {
     return Column(
       children: [
         SizedBox(
-            // Ukuran Card
-            height: 150,
-            child: Card(
-              clipBehavior: Clip.antiAlias,
-              shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                Radius.circular(20.0),
-              )),
-              margin: const EdgeInsets.all(10),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                              const DetailItemPage()));
-                },
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.all(10),
-                        decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image:
-                                  AssetImage('assets/material/bouquet-1.png'),
-                              fit: BoxFit.cover,
-                            ),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                      ),
-                    ),
-                    Expanded(
-                        child: Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      child: const Text('Bouquet'),
-                    ))
-                  ],
-                ),
-              ),
+          // Ukuran Card
+          height: 150,
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+              Radius.circular(20.0),
             )),
+            margin: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/material/bouquet-1.png'),
+                          fit: BoxFit.cover,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                  ),
+                ),
+                Expanded(
+                    child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: const Text('Bouquet'),
+                ))
+              ],
+            ),
+          ),
+        ),
         SizedBox(
-            // Ukuran Card
-            height: 150,
-            child: Card(
-              clipBehavior: Clip.antiAlias,
-              shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                Radius.circular(20.0),
-              )),
-              margin: const EdgeInsets.all(10),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                              const DetailItemPage()));
-                },
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.all(10),
-                        decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image:
-                                  AssetImage('assets/material/bouquet-1.png'),
-                              fit: BoxFit.cover,
-                            ),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                      ),
-                    ),
-                    Expanded(
-                        child: Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      child: const Text('Bouquet'),
-                    ))
-                  ],
-                ),
-              ),
+          // Ukuran Card
+          height: 150,
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+              Radius.circular(20.0),
             )),
+            margin: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/material/bouquet-1.png'),
+                          fit: BoxFit.cover,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                  ),
+                ),
+                Expanded(
+                    child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: const Text('Bouquet'),
+                ))
+              ],
+            ),
+          ),
+        ),
         SizedBox(
-            // Ukuran Card
-            height: 150,
-            child: Card(
-              clipBehavior: Clip.antiAlias,
-              shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                Radius.circular(20.0),
-              )),
-              margin: const EdgeInsets.all(10),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                              const DetailItemPage()));
-                },
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.all(10),
-                        decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image:
-                                  AssetImage('assets/material/bouquet-1.png'),
-                              fit: BoxFit.cover,
-                            ),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                      ),
-                    ),
-                    Expanded(
-                        child: Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      child: const Text('Pink Rose Bouquet'),
-                    ))
-                  ],
-                ),
-              ),
+          // Ukuran Card
+          height: 150,
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+              Radius.circular(20.0),
             )),
+            margin: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/material/bouquet-1.png'),
+                          fit: BoxFit.cover,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                  ),
+                ),
+                Expanded(
+                    child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: const Text('Pink Rose Bouquet'),
+                ))
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
